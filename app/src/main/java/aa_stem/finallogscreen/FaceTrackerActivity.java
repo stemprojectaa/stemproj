d9d8eb84168d0f588e2503a8a11a669a8b7afed9
Abdullah <an153an@gmail.com>
2018-01-23 21:32:24 -0800

    jan23commit

---
+++ app/src/main/java/aa_stem/finallogscreen/FaceTrackerActivity.java
@@ -0,0 +1,8 @@
+package aa_stem.finallogscreen;
+
+/**
+ * Created by mnaveed on 1/3/18.
+ */
+
+public class FaceTrackerActivity {
+}
---
+++ app/src/main/java/aa_stem/finallogscreen/ImageRecognition.java
@@ -0,0 +1,13 @@
+package aa_stem.finallogscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageRecognition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_recognition);
+    }
+}
